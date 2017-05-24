70ae9aa7d478c50c6a1474bd84d5b57048655edf
Sabbir Ahmed <sabbir.eee.hstu@gmail.com>
2017-05-25 04:50:03 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,11 @@ echo
 echo "installing..."
 sudo cp alart.mp3 /opt
 sudo cp selfMode /bin
-sudo chmod 755 selfMode
+sudo chmod 755 /bin/selfMode
+echo
+cd ..
+sudo rm -rf selfMode
+echo "Removing files"
 echo
 sleep 2
 echo "Done!"
